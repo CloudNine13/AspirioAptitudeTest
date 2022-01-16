3fd7394fac5f50aad597d56f431e19b0f17e146a
Igor Dzichkovskii <igor@vitcord.com>
2022-01-16 23:40:45 +0100

    minor changes

--- react-ui/src/helpers/Socket.js
+++ react-ui/src/helpers/Socket.js
@@ -12,7 +12,7 @@ import React from "react";
 const PORT = process.env.PORT || 8080
 
 export const socket = () => {
-    const SERVER = `http://localhost:${PORT}/`
+    const SERVER = `https://aspirio-aptitude-test.herokuapp.com:${PORT}/`
     const io = require("socket.io-client")
     return io.connect(SERVER)
 }
